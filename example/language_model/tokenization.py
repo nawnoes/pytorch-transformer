91034a5f5d117ce0c81a972a062c23518ebd55b6
nawnoes <nawnoes@gmail.com>
2021-03-09 21:31:59 +0900

    for colab training

---
+++ example/language_model/tokenization.py
@@ -0,0 +1,7 @@
+def whitespace_tokenize(text):
+    """Runs basic whitespace cleaning and splitting on a piece of text."""
+    text = text.strip()
+    if not text:
+        return []
+    tokens = text.split()
+    return tokens
